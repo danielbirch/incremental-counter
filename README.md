f8724851e2ec964b151bdcc6c7a5af88f1a0ab3b
Daniel Birch <daniel@danielbirch.dev>
2023-03-30 03:08:18 +0300

    First upload of project files, finished and functional app.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # incremental-counter
+
 A basic incremental and decremental counter.
+
+I built this counter by myself without the use of any tutorials, just a bunch of research and troubleshooting. This incremental counter allows the user to count up and down, reset the display, save a number in local storage and then load that value at any time. When a value is loaded, the user can continue incrementing from that same value.
---
+++ counter.js
@@ -0,0 +1,61 @@
+// SELECTORS
+const minus = document.getElementById('decrement');
+const plus = document.getElementById('increment');
+const doReset = document.getElementById('reset');
+const doSave = document.getElementById('save');
+const count = document.getElementById('current-count');
+const doload = document.getElementById('load-count');
+
+// EVENT LISTENERS
+minus.addEventListener('click', decrement);
+plus.addEventListener('click', increment);
+doReset.addEventListener('click', reset);
+doSave.addEventListener('click', save);
+doload.addEventListener('click', load);
+
+// FUNCTIONS
+// Set initial value
+let currentCount = 0;
+let storedCount = 0;
+// Reduce current count by 1 when decrement button is clicked
+function decrement() {
+  currentCount--;
+  count.innerText = currentCount;
+  localStorage.setItem('currentCount', currentCount);
+}
+// Increase current count by 1 when increment button is clicked
+function increment() {
+  currentCount++;
+  count.innerText = currentCount;
+  localStorage.setItem('currentCount', currentCount);
+}
+// Reset the value back to 0 when reset button is clicked
+function reset() {
+  currentCount = 0;
+  storedCount = 0;
+  count.innerText = currentCount;
+  // Set currentCount to keep up with the increments
+  localStorage.setItem('currentCount', currentCount);
+}
+
+// Save value to local storage when save button is clicked
+function save() {
+  // Check if anything is in local storage
+  let storedCount = localStorage.getItem('storedCount');
+  if (storedCount === null) {
+    storedCount = currentCount.toString();
+  } else {
+    storedCount = (parseInt(currentCount)).toString();
+  }
+  localStorage.setItem('storedCount', storedCount);
+}
+
+// Get value from local storage and display as counter number
+function load() {
+  // Get stored value and set as a variable
+  let loadCount = localStorage.getItem('storedCount');
+  // Set stored count as the current count
+  localStorage.setItem('currentCount', loadCount);
+  currentCount = parseInt(loadCount);
+  count.innerText = loadCount;
+}
